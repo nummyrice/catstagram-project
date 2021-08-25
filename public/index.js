f82090ae3e5bc1e86ee75aa183c1f4ca9c912252
nummyrice <nicholasroyrice@gmail.com>
2021-08-25 11:00:49 -0700

    we added a popularity div

--- public/index.js
+++ public/index.js
@@ -1,14 +1,25 @@
 let catImage;
 window.onload = () => {
+    //interacts with API and gets random cat image
     fetch("https://api.thecatapi.com/v1/images/search", {"method": "GET", "header": {"x-api-key": "cc6decfa-0715-4d20-8982-75fc3d73f806"}})
     .then(res => res.json())
     .then(ans => {
+        //creates image element using the retrieved image url
         catImage = document.createElement("img");
         catImage.setAttribute("src", ans[0].url);
         box.appendChild(catImage);
         catImage.className = "catImage";
         box.setAttribute("id", "mainBox")
         document.body.appendChild(box);
+        //creates new div in the box div that the image is in for popularity score
+        let popularityCounterDiv = document.createElement("div");
+        popularityCounterDiv.setAttribute("class", "popularity");
+
+        popularityCounterDiv.innerText = "Popularity Score: 0";
+        box.appendChild(popularityCounterDiv);
+        console.log(popularityCounterDiv);
+
+
     })
     .catch(e => console.log(e))
     let box = document.createElement("div");
@@ -18,4 +29,14 @@ window.onload = () => {
     title.setAttribute("class", "title");
     box.appendChild(title);
 
+    let popularityScore = 0;
+
+
+
+    // let popularityLabel = document.createElement("label");
+
+    // document.getElementsByClassName("popularity").appendChild(popularityCounterDiv);
+
 }
+
+// window.addEventListener("DOMContentLoaded", )
